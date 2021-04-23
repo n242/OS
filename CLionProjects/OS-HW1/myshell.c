373f1f0021bec299a569c4f33f87027c6cb6b89d
net242 <neta5128@gmail.com>
2021-04-23 09:31:17 +0300

    final file sent and zip

--- CLionProjects/OS-HW1/myshell.c
+++ CLionProjects/OS-HW1/myshell.c
@@ -28,22 +28,16 @@ int main(void)
 
         command[strcspn(command, "\n")] = 0; //removing /n from final string
         int run_at_bg = 0;//adding flag to check if to run process at bg
-        int j;
 
-        char* location = strstr(command, "&");
+        char* location = strstr(command, "&"); //checking if to run at bg
         if(location!=NULL){
-            //printf("location is: %s\n" ,location);
-            //printf("in run at bg\n");
             run_at_bg = 1;
             command[strcspn(command, " &")] = 0; //removing final &
-           // printf("run at bg is: %d\n", run_at_bg);
         }
-
-       // printf("run at bg is: %d\n", run_at_bg);
 
         char *argv[BUFFER_SIZE];
         *argv = command; //initializing location of argv
-        int i=0;
+        int i=0;//number of arguments
         int status;
         char *tmp = strtok(command ," ");//strok returns ptr to first argument in string
 
@@ -58,21 +52,16 @@ int main(void)
         argv[i] = NULL;
         int len =i;
 
-
-        printf("i is: %d\n", i);
-        //for(;i>=0; i--)
-         //   printf("argv[%d] is: %s \n",i, argv[i]);
         pid_t pid = fork(); //fork a child process
-        if(pid<0){
-            printf("ERROR: fork failed\n");
-            exit(1);
-        }
-        else if(pid==0 || run_at_bg==1){
-            for(;i>=0; i--)
-                printf("argv[%d] is: %s \n",i, argv[i]);
-            if(execvp(argv[0], argv) <0) {//first arg is * second is **
-                printf(("ERROR: execvp failed\n"));
-                }
+        //if(pid<0){
+            //printf("ERROR: fork failed\n");
+        //    exit(1);
+        //}
+        if(pid==0 || run_at_bg==1){
+            execvp(argv[0], argv);//first arg is * second is **
+            //if(execvp(argv[0], argv) <0) {
+             //   printf(("ERROR: execvp failed\n"));
+            //    }
                 exit(1);
             }
         if(run_at_bg==1){
@@ -81,11 +70,8 @@ int main(void)
         else{ //waiting while pid>0
             while (wait(&status) != pid)
                 ;
-                //return status;
         }
-
     }//end while 1
 
-        //printf("This line will not be printed if execvp() runs correctly or after exit\n");
     return 0;
 }
